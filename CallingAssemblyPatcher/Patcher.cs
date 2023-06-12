37a9237902525c1fe4175bcfe462e04cfcaa9107
Almond <Almond@DESKTOP-7LTOP2J>
2023-06-13 00:57:32 +0800

    Add project files.

---
+++ CallingAssemblyPatcher/Patcher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace CallingAssemblyPatcher
+{
+    internal class Patcher
+    {
+        public void Patch(ModuleDefMD module)
+        {
+            int dc = 0;
+            foreach (TypeDef type in module.GetTypes().Where(t => t.HasMethods))
+            {
+                foreach (MethodDef method in type.Methods.Where(m => m.HasBody && m.Body.HasInstructions))
+                {
+                    var instr = method.Body.Instructions;
+                    
+                    foreach (var t in instr)
+                    {
+                        if (t.OpCode.Code == Code.Call &&
+                            t.Operand.ToString().Contains("GetCallingAssembly"))
+                        {
+                            dc++;
+                            t.Operand = module.Import(typeof(Assembly).GetMethod("GetExecutingAssembly"));
+                        }
+                    }
+                }
+            }
+
+            if (dc == 0)
+            {
+                Console.WriteLine("No instructions to be patched");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"Patched: {dc}");
+            }
+        }
+    }
+}
---
+++ CallingAssemblyPatcher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+
+namespace CallingAssemblyPatcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath;
+            try
+            {
+                filePath = args[0];
+            }
+            catch
+            {
+                Console.WriteLine("Enter file path to patch:");
+                filePath = Console.ReadLine();
+            }
+
+            ModuleDefMD module = ModuleDefMD.Load(filePath);
+
+            new Patcher().Patch(module);
+
+            var opts = new ModuleWriterOptions(module);
+            opts.MetadataLogger = DummyLogger.NoThrowInstance; //Prevents errors if module is obfuscated
+            string output = Path.GetFileNameWithoutExtension(filePath) + "_patched" + Path.GetExtension(filePath);
+            module.Write(output, opts);
+        }
+    }
+}
